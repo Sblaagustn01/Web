0fb60c57165493f53be795537288a2a5f5d200ab
Sblaagustn01 <141691819+Sblaagustn01@users.noreply.github.com>
2023-10-10 14:06:22 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,11 @@
+let nama = "Sabila Agustina";
+document.getElementById("nama").innerHTML = nama
+
+let alamat = "Kp.Purbasari";
+document.getElementById("alamat").innerHTML = alamat
+
+let ttg = "Tasikmalaya, 1 Agustus 2007";
+document.getElementById("ttg").innerHTML = ttg
+
+let hoby = "Ngamer";
+document.getElementById("hoby").innerHTML = hoby
---
+++ perhitungan2.js
@@ -0,0 +1,15 @@
+let angka = 5 + 3;
+document.writeln("<P>5 + 3 = " + angka + "</P>")
+originalAngka = angka;
+
+angka = 5 - 3;
+document.writeln("<p>5 - 3 = " + angka + "</p>")
+originalAngka = angka;
+
+angka = 5 * 3;
+document.writeln("<p>5 * 3 = " + angka + "</p>")
+originalAngka = angka;
+
+angka = 5 / 3;
+document.writeln("<p>5 / 3 = " + angka + "</p>")
+originalAngka = angka;
